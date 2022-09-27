e125988035ee56a577b7b4d5494d32e15bd4d9db
Ezequiel Silva <l.ezequielsilva89@gmail.com>
2022-09-26 22:55:39 -0300

    primera pre-entrega JS

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
 
 
-// Para mi trabajo final deseo realizar una maqueta tipo E-commerce 
-// para ello, necesitare que la persona ingrese un usuario y contraseña para
-// poder operar.
+//  Para mi trabajo final deseo realizar una maqueta tipo E-commerce 
+//  para ello, necesitare que la persona ingrese un usuario y contraseña para
+//  poder operar.
 
-// 1) crear un algoritmo con un condicional:
-
-
-const usuarioAutorizado = "ezequiel";
-const passwordAutorizado = "silva";
-
-let usuarioIngresado = prompt("Ingrese su nombre de usuario: "); 
-let passwordIngresado = prompt("Ingrese su contraseña: "); 
+      //  1) crear un algoritmo con un condicional, Ejemplo 1:
 
 
-for(let i = 0; i < 2; i++) {
-    if(usuarioIngresado === usuarioAutorizado && passwordIngresado === passwordAutorizado) {
-        alert("contraseña correcta, Bienvenido!");
-        break;
+// const usuarioAutorizado = "ezequiel";
+// const passwordAutorizado = "silva";
+
+// let usuarioIngresado = prompt("Ingrese su nombre de usuario: "); 
+// let passwordIngresado = prompt("Ingrese su contraseña: "); 
+
+
+// for(let i = 0; i < 2; i++) {
+//     if(usuarioIngresado === usuarioAutorizado && passwordIngresado === passwordAutorizado) {
+//         alert("contraseña correcta, Bienvenido!");
+//         break;
         
-    }else {
-         alert("contraseña o usuario incorrecto, volvé a intentarlo");
-         break;
-                
-     } 
-    }
+//     }else {
+//          alert("contraseña o usuario incorrecto, volvé a intentarlo");
+//          break;
+            
+//      } 
+//     }
 
 
-// Ejemplo 2
+       // Ejemplo 2 :
+
 // siguiendo con la tematica de mi proyecto final, la persona podrá tener la opcion 
 // de calcular el " costo de envío por zona" para ello crearé una funcion
 // que la pueda utilizar cada vez que se requiera: 
@@ -60,7 +61,7 @@ switch(localidad) {
 
 }
 
-calcularCostoDeEnvio();
+// calcularCostoDeEnvio();
 
 
 
