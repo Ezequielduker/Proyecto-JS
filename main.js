19383a0a9718d95d37dc7838d7c9b70e058b65df
Ezequiel Silva <l.ezequielsilva89@gmail.com>
2022-10-28 00:14:40 -0300

    Tercera pre entrega JS.

--- main.js
+++ main.js
@@ -1,107 +1,3 @@
-// const productos = [
-//     { nombre: "celular", precio: 100 },
-//     { nombre: "tablet", precio: 200 },
-//     { nombre: "termo", precio: 50 },
-//     { nombre: "notebook", precio: 250 },
-//     { nombre: "funda", precio: 150 },
-// ];
-
-// let carrito = []
-
-// let seleccion = prompt("¿ Hola desea comprar algun producto ? Responda si o no")
-
-// while (seleccion != "si" && seleccion != "no") {
-//     alert("Por favor Ingresa si o no")
-//     seleccion = prompt("Hola desea comprar, Responda  si o no")
-// }
-
-// if (seleccion == "si") {
-//     alert("A continuación nuestra lista de productos, elegí algunas de las opciones: ")
-//     let todosLosProductos = productos.map((producto) => producto.nombre + "" + producto.precio + "$")
-//     alert(todosLosProductos.join())
-// } else if (seleccion == "no") {
-//     alert("Muchas Gracias, Nos vemos luego! ")
-// }
-
-// // para elegir productos.
-
-// while (seleccion != "no") {
-//     let producto = prompt("Agregá un producto a tu carrito")
-//     let precio = 0
-
-//     if (producto == "celular" || producto == "tablet" || producto == "termo" || producto == "notebook" ||
-//         producto == "funda") {
-//         switch (producto) {
-//             case "celular":
-//                 precio = 100;
-//                 break;
-
-//             case "tablet":
-//                 precio = 200;
-//                 break;
-
-//             case "termo":
-//                 precio = 50;
-//                 break;
-
-//             case "notebook":
-//                 precio = 250;
-//                 break;
-//             case "funda":
-//                 precio = 200;
-//                 break;
-//             default:
-//                 break;
-//         }
-
-//         let unidades = parseInt(prompt("cuantas unidades quiere llevar"))
-
-//         carrito.push({ producto, unidades, precio })
-//         console.log(carrito)
-//     } else {
-//         alert("¡No tenemos ese producto!")
-//     }
-
-//     seleccion = prompt("¿ Desea seguir comprando ?")
-//     while (seleccion === "no") {
-//         alert("¡Gracias por la compra!")
-//         carrito.forEach((carritoFinal) => {
-//             console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades},
-//             total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`)
-//         })
-//         break;
-//     }
-// }
-// // suma el valor total de los productos elegidos.
-// const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
-// console.log(`el total a pagar por su compra es: ${total}`);
-
-// // BUSCADOR.
-
-// const producto = document.getElementById("articulosCarrito");
-
-// const catalogo = () => {
-//     for (let producto of productos) {
-//         producto.innerHTML += `
-                
-
-//                 <div class="card" style="width: 18rem;" id="articulosCarrito">
-//                 <img src="./imagenes/img1--tarjeta.jpg" class="card-img-top" alt="...">
-//                 <div class="card-body">
-//                 <h5 class="card-title">Nombre: ${producto.nombre}</h5>
-//                     <p class="card-text">Valor: ${producto.precio}</p>
-//                     </div>
-//                     <div class="button--container">
-//                         <button class="button">AGREGAR</button>
-//                     </div>
-
-                                       
-// //         `
-        
-//     }
-// }
-
-
 
 // Variables
 
@@ -115,9 +11,12 @@ cargarEventListeners();
     function cargarEventListeners() {
         listaTelefonos.addEventListener('click', agregarCurso);
 
-         // muestra los cursos LS
+        // elimino del carrito
+        carrito.addEventListener('click', eliminarCurso);
+
+         // muestra los telefonos LS
          document.addEventListener('DOMContentLoaded',() =>{
-            articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+            articulosCarrito = JSON.parse( localStorage.getItem('carrito')) || [];
             carritoHTML();
         })
         // vaciar carrito
@@ -141,9 +40,19 @@ function agregarCurso(e) {
    
 }
 
+// elimino productos del carrito
+function eliminarCurso(e) {
+    console.log(e.target.classList);
+    if(e.target.classList.contains('borrar-curso')) {
+       const cursoId = e.target.getAttribute('data-id');
+       articulosCarrito = articulosCarrito.filter( curso => curso.id !== cursoId);
+       carritoHTML();
+    }
+}
+
 // lee el contenido del html al que le dimos click y extrae la info
 function leerDatosCurso(curso) {
-    // console.log(curso);
+    
     // crear un objeto con el contenido de las ventas
     const infoVenta = {
        precio: curso.querySelector('p').textContent,
@@ -155,28 +64,24 @@ function leerDatosCurso(curso) {
 
     // revisa si un elemento ya existe
 
-    const existe = articulosCarrito.some( curso => curso.id === infoVenta.id);
-    if(existe){
-        // actualizamos
-        const cursos = articulosCarrito.map ( curso => {
-            if(curso.id === infoVenta.id ){
+    const existe = articulosCarrito.some( curso => curso.id === infoVenta.id );
+    if(existe) {
+        const cursos = articulosCarrito.map( curso => {
+            if( curso.id === infoVenta.id ) {
                 curso.cantidad++;
                 return curso;
-            }else {
+            } else {
                 return curso;
             }
-            });
-
-            articulosCarrito = [...cursos];
-    }else {
-        // agregamos
-        articulosCarrito = [...articulosCarrito, infoVenta ];
+        });
+        articulosCarrito =  [...cursos];
+    } else {
+        articulosCarrito =  [...articulosCarrito, infoVenta];
     }
-
-    // agrega elementos al arreglo del carrito
+  
+           
+            
     
-    articulosCarrito = [...articulosCarrito, infoVenta ];
-
     console.log(articulosCarrito);
     carritoHTML();
 
@@ -222,9 +127,11 @@ function carritoHTML() {
 
     // storage
     sincronizarStorage();
-    function sincronizarStorage(){
-        localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
-    }
+    
+}
+
+function sincronizarStorage(){
+    localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
 }
 
 // elimina los cursos
